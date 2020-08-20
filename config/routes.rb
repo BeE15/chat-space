ffde797421fa5367697c25e510fe96d88a0bebc9
BeE15 <sotaimo15@gmail.com>
2020-08-20 21:40:50 +0900

    Revert "test"
    This reverts commit af0dd1409b662ed04e1542336689c92ea917c044.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'messages#index'
-  resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create]
+  root "messages#index"
 end
